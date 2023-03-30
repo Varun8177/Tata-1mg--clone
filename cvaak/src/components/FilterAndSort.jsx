ec1ab397ac8196d5aa531af9359cd766f3dd585e
varun8177 <varunergurala9999@gmail.com>
2023-03-30 23:51:04 +0530

    products and single product page UI created

---
+++ cvaak/src/components/FilterAndSort.jsx
@@ -0,0 +1,254 @@
+import { sortData } from "@/pages/api/hello";
+import { FilterDataReq, getData } from "@/redux/products/products.action";
+import { REQUEST_FILTER } from "@/redux/products/products.actionTypes";
+import {
+  Box,
+  Button,
+  Checkbox,
+  CheckboxGroup,
+  Divider,
+  Heading,
+  Hide,
+  Radio,
+  RadioGroup,
+  Show,
+  Stack,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
+const FilterAndSort = ({
+  handleSortPrice,
+  handleSortRating,
+  handleSortReset,
+}) => {
+  const router = useRouter();
+  const btnRef = React.useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const dispatch = useDispatch();
+  const [filterValues, setFilterValues] = useState([]);
+  const [sort, setSort] = useState("");
+  const [Rating, setRating] = useState("");
+  const handleFilterChange = (value) => {
+    setFilterValues(value);
+  };
+
+  const handleReset = () => {
+    dispatch(getData("", "", []));
+  };
+
+  useEffect(() => {
+    let params = {};
+    if (filterValues.length) params.category = filterValues;
+    router.push({
+      query: params,
+    });
+    dispatch(getData("", "", filterValues));
+  }, [filterValues]);
+
+  useEffect(() => {
+    let params = {};
+    if (sort) {
+      params.sortBy = "price";
+      params.order = sort;
+    }
+    router.push({
+      query: params,
+    });
+    dispatch(getData(params.sortBy, sort, filterValues));
+  }, [sort]);
+
+  useEffect(() => {
+    let params = {};
+    if (sort) {
+      params.sortBy = "rating";
+      params.order = Rating;
+    }
+    router.push({
+      query: params,
+    });
+    dispatch(getData(params.sortBy, Rating, filterValues));
+  }, [Rating]);
+  return (
+    <>
+      <Show above="sm">
+        <Box
+          bg="white"
+          w={{ lg: "190px", md: "250px" }}
+          h={{ lg: "500px", md: "500px" }}
+          ml={{ lg: "7%", md: "5%" }}
+          mt="15px"
+          p={5}
+          fontSize={{ lg: "14px", md: "10px" }}
+        >
+          <Box m="20px 0">
+            <Heading fontSize={"14px"}>FILTER</Heading>
+            <CheckboxGroup
+              colorScheme="green"
+              value={filterValues}
+              onChange={handleFilterChange}
+            >
+              <Stack color={"grey"} spacing={[1, 1]} direction={["column"]}>
+                <Checkbox fontSize="10px" value="calcium">
+                  Calcium
+                </Checkbox>
+                <Checkbox value="supplement">Suppliments</Checkbox>
+                <Checkbox value="aryurvedic">Proteins</Checkbox>
+                <Checkbox value="mask">masks</Checkbox>
+              </Stack>
+            </CheckboxGroup>
+          </Box>
+          <Divider />
+          <Box m="20px 0">
+            <Heading fontSize={"14px"}>SORT BY PRICE</Heading>
+            <RadioGroup
+              colorScheme="green"
+              onChange={(value) => {
+                setSort(value);
+              }}
+              value={sort}
+            >
+              <Stack color={"grey"} direction="column" fontSize={"12px"}>
+                <Radio value="asc">Low to high</Radio>
+                <Radio value="des">High to low</Radio>
+              </Stack>
+            </RadioGroup>
+          </Box>
+          <Divider />
+          <Box m="20px 0">
+            <Heading fontSize={"14px"}>SORT BY RATING</Heading>
+            <RadioGroup
+              colorScheme="green"
+              onChange={(value) => {
+                setRating(value);
+              }}
+              value={Rating}
+            >
+              <Stack color={"grey"} direction="column" fontSize={"12px"}>
+                <Radio value="asc">Low to high</Radio>
+                <Radio value="des">High to low</Radio>
+              </Stack>
+            </RadioGroup>
+          </Box>
+          <Button
+            fontWeight={"700"}
+            bg="white"
+            size={"sm"}
+            color="#ff6f61"
+            _hover={{
+              bg: "#ff6f61",
+              color: "white",
+            }}
+            onClick={() => {
+              setFilterValues([]);
+              handleReset();
+              setSort("");
+              setRating("");
+            }}
+          >
+            Reset
+          </Button>
+        </Box>
+      </Show>
+
+      {/* -------------------- */}
+
+      <Hide above="sm">
+        <>
+          <Button ref={btnRef} colorScheme="gray" onClick={onOpen}>
+            Filter and Sort
+          </Button>
+          <Drawer
+            isOpen={isOpen}
+            placement="bottom"
+            onClose={onClose}
+            finalFocusRef={btnRef}
+          >
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerBody>
+                <Box>
+                  <Box>
+                    <Heading fontSize={"14px"} mt="20px">
+                      FILTER
+                    </Heading>
+                    <CheckboxGroup
+                      colorScheme="green"
+                      value={filterValues}
+                      onChange={handleFilterChange}
+                    >
+                      <Stack
+                        color={"grey"}
+                        fontSize={"12px"}
+                        spacing={[1, 1]}
+                        direction={["column"]}
+                      >
+                        <Checkbox fontSize="10px" value="calcium">
+                          Calcium
+                        </Checkbox>
+                        <Checkbox value="Suppliments">Suppliments</Checkbox>
+                        <Checkbox value="Proteins">Proteins</Checkbox>
+                        <Checkbox value="masks">masks</Checkbox>
+                      </Stack>
+                    </CheckboxGroup>
+                  </Box>
+                  <Box m="10px 0">
+                    <Heading fontSize={"14px"}>SORT BY PRICE</Heading>
+                    <RadioGroup colorScheme="green">
+                      <Stack
+                        color={"grey"}
+                        direction="column"
+                        fontSize={"12px"}
+                        onChange={(value) => handleSortPrice(value)}
+                      >
+                        <Radio value="asc">Low to high</Radio>
+                        <Radio value="desc">High to low</Radio>
+                      </Stack>
+                    </RadioGroup>
+                  </Box>
+                  <Divider />
+                  <Box m="10px 0">
+                    <Heading fontSize={"14px"}>SORT BY RATING</Heading>
+                    <RadioGroup colorScheme="green">
+                      <Stack
+                        color={"grey"}
+                        direction="column"
+                        fontSize={"12px"}
+                        onChange={(value) => handleSortRating(value)}
+                      >
+                        <Radio value="asc">Low to high</Radio>
+                        <Radio value="desc">High to low</Radio>
+                      </Stack>
+                    </RadioGroup>
+                  </Box>
+                  <Button
+                    fontWeight={"700"}
+                    bg="white"
+                    size={"sm"}
+                    color="#ff6f61"
+                    _hover={{
+                      bg: "#ff6f61",
+                      color: "white",
+                    }}
+                    onClick={() => handleSortReset()}
+                  >
+                    Reset
+                  </Button>
+                </Box>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
+        </>
+      </Hide>
+    </>
+  );
+};
+export default FilterAndSort;
--- cvaak/src/redux/store.js
+++ cvaak/src/redux/store.js
@@ -1,7 +1,10 @@
 import { applyMiddleware, combineReducers, compose, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
+import ProductReducer from "./products/products.reducer"
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+    ProductReducer
+})
 
 const store = legacy_createStore(rootReducer, compose(applyMiddleware(thunk)))
 
